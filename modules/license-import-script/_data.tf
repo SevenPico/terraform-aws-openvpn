fb6b45057bf940d960c5f090def09ac0175fd6b9
Dave Raring <d@7pi.co>
2022-09-07 14:43:34 -0500

    fixing context

--- modules/license-import-script/_data.tf
+++ modules/license-import-script/_data.tf
@@ -1,10 +1,16 @@
-
+# The AWS region currently being used.
 data "aws_region" "current" {
-  count = module.this.enabled ? 1 : 0
 }
 
+# The AWS account id
 data "aws_caller_identity" "current" {
-  count = module.this.enabled ? 1 : 0
+}
+
+# The AWS partition (commercial or govcloud)
+data "aws_partition" "current" {}
+
+locals {
+  arn_prefix = "arn:${data.aws_partition.current.partition}"
 }
 
 
