7b759e5ee60e3307603d5d37fc05c5a5906607cb
Dave Raring <d@7pi.co>
2022-10-24 11:19:37 -0500

    Separating NAT and Reverse Routing configurations

---
+++ modules/nat-routing-script/_data.tf
@@ -0,0 +1,16 @@
+# The AWS region currently being used.
+data "aws_region" "current" {
+}
+
+# The AWS account id
+data "aws_caller_identity" "current" {
+}
+
+# The AWS partition (commercial or govcloud)
+data "aws_partition" "current" {}
+
+locals {
+  arn_prefix = "arn:${data.aws_partition.current.partition}"
+}
+
+
--- modules/nat-routing-script/main.tf
+++ modules/nat-routing-script/main.tf
@@ -1,15 +1,15 @@
-module "reverse_routing_sh_context" {
+module "nat_routing_sh_context" {
   source     = "app.terraform.io/SevenPico/context/null"
   version    = "1.0.2"
   context    = module.context.self
-  attributes = ["reverse", "routing"]
+  attributes = ["nat", "routing"]
 }
 
-resource "aws_s3_object" "reverse_routing_sh" {
-  count  = module.reverse_routing_sh_context.enabled ? 1 : 0
+resource "aws_s3_object" "nat_routing_sh" {
+  count  = module.nat_routing_sh_context.enabled ? 1 : 0
   bucket = var.bucket_id
   key    = "reverse-routing.sh"
-  content = templatefile("${path.module}/reverse-routing.sh.tftpl", {
+  content = templatefile("${path.module}/nat-routing.sh.tftpl", {
 #    client_dhcp_network          = var.openvpn_client_dhcp_network,
 #    client_dhcp_network_mask     = var.openvpn_client_dhcp_network_mask,
     openvpn_client_cidr_blocks   = join(" ", var.openvpn_client_cidr_blocks),
