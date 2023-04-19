583bcc141fa94cc504d6103d8c49f88e633c65e6
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-04-19 20:29:37 +0530

    removed locals cloudflare cidr

--- examples/default/openvpn.tf
+++ examples/default/openvpn.tf
@@ -29,26 +29,6 @@ module "openvpn_context" {
   name    = "vpn"
 }
 
-locals {
-  cloudflare_cidrs = [
-    "103.21.244.0/22",
-    "103.22.200.0/22",
-    "103.31.4.0/22",
-    "104.16.0.0/13",
-    "104.24.0.0/14",
-    "108.162.192.0/18",
-    "131.0.72.0/22",
-    "141.101.64.0/18",
-    "162.158.0.0/15",
-    "172.64.0.0/13",
-    "173.245.48.0/20",
-    "188.114.96.0/20",
-    "190.93.240.0/20",
-    "197.234.240.0/22",
-    "198.41.128.0/17"
-  ]
-}
-
 
 # ------------------------------------------------------------------------------
 # Openvpn IAM Role Policy Doc
@@ -136,18 +116,6 @@ module "openvpn" {
       self                     = null
       description              = "Allow https egress to VPC."
     },
-    {
-      key                      = "egress-to-cloudflare"
-      type                     = "egress"
-      from_port                = 443
-      to_port                  = 443
-      protocol                 = "tcp"
-      cidr_blocks              = local.cloudflare_cidrs
-      ipv6_cidr_blocks         = []
-      source_security_group_id = null
-      self                     = null
-      description              = "Allow https egress to Cloudflare."
-    }
   ]
   ec2_additional_instance_role_policies = data.aws_iam_policy_document.openvpn_ec2_policy_doc[*].json
 
