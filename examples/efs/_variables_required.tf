0e933be45333e0f148a617a48f89b5811fdb5455
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-05-09 23:39:57 +0530

    fixed all the examples

--- examples/efs/_variables_required.tf
+++ examples/efs/_variables_required.tf
@@ -19,6 +19,12 @@
 ##  This file contains code written by SevenPico, Inc.
 ## ----------------------------------------------------------------------------
 
-variable "vpc_cidr_block" { default = "10.10.0.0/16" }
-variable "availability_zones" { default = ["us-east-1a", "us-east-1b", "us-east-1c"] }
-variable "root_domain" { default = "7pi.io" }
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "availability_zones" {
+  type = list(string)
+}
+variable "root_domain" {
+  type = string
+}
